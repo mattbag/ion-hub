6a7d56eb079f06a26e6c945eb48b4fe1d15ab430
matteo <matteo@atlasagency.com.au>
2017-02-09 15:57:56 +1100

    static dist

--- README.md
+++
@@ -1,3 +0,0 @@
-# ion-hub
-
-## Just trying the amazing ionic with the github api.
--- src/models/org.ts
+++
@@ -1,8 +0,0 @@
-export interface Org{
-    login: string,
-    url: string,
-    repos_url: any,
-    events_url:string,
-    description: string
-
-}
